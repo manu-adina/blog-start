532de77401e445d8a1b55f71525a6209413099f6
Manu Adina <manu.adina@remsense.com.au>
2025-10-09 09:17:22 +0800

    REMSEN-26 cleaned up files

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,20 @@ import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 import { cloudflare } from '@cloudflare/vite-plugin'
+import mdx from '@mdx-js/rollup'
 
 const config = defineConfig({
-  plugins: [
-    cloudflare({ viteEnvironment: { name: 'ssr' } }),
-    // this is the plugin that enables path aliases
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
-    tailwindcss(),
-    tanstackStart(),
-    viteReact(),
-  ],
+    plugins: [
+        cloudflare({ viteEnvironment: { name: 'ssr' } }),
+        // this is the plugin that enables path aliases
+        viteTsConfigPaths({
+            projects: ['./tsconfig.json'],
+        }),
+        tailwindcss(),
+        tanstackStart(),
+        mdx(),
+        viteReact(),
+    ],
 })
 
 export default config
